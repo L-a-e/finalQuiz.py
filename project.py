c3e73380bf21c8a81d80fc2cac81a08db3ee410a
Andrew Lay <andrewlaymac@gmail.com>
2025-05-11 16:28:19 -0500

    set the while look to lock at the fps which is 60 so that the app runs consistently across all platforms.

--- project.py
+++ project.py
@@ -6,8 +6,10 @@ WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Drawing Program")
 
 run = True
-
+clock = pygame.time.Clock()
 while run:
+    clock.tick(FPS)
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
