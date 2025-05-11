9a217585a33f0f28878248e304abc24b4a0ee348
Andrew Lay <andrewlaymac@gmail.com>
2025-05-11 18:59:03 -0500

    created a class in button folder

---
+++ utils/button.py
@@ -0,0 +1,11 @@
+from .settings import * 
+
+class Button:
+    def __init__(self, x, y, width, height,color, text=None, text_color=BLACK):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.color = color
+        self.text = text
+        self.text_color = text_color
