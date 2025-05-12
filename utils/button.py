7f41ac80fab66d5aeaaca789d68e26f1109e1d64
Andrew Lay <andrewlaymac@gmail.com>
2025-05-11 19:19:47 -0500

    in the def_ clicked function the x and y positions are checked to see if the pos clicked is within the x and y of the actual button being clicked, if it isn't in those bounds return false.

--- utils/button.py
+++ utils/button.py
@@ -19,4 +19,11 @@ class Button:
             win.blit(text_surface, (self.x + self.width / 2 - text_surface.get_width()/2,
                                     self.y + self.height / 2 - text_surface.get_height()/2,))
     def clicked(self, pos):
-        pass
+        x, y = pos
+
+        if not (x >= self.x and x <= self.x + self.width):
+            return False
+        if not (y >= self.y and y <= self.y + self.height):
+            return False
+        
+        return True
